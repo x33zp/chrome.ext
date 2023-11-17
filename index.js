d03fd2b5070dd5c4954ac61e723b9860d529294e
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-17 16:35:19 +0100

    logging out 'Button clicked' to test if javaScript button function works

---
+++ index.js
@@ -0,0 +1,7 @@
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function () {
+    console.log("Button clicked!")
+})
