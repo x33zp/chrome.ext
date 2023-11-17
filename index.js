08b06171248d431558e35be69cefc73dd4abc268
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-17 17:03:02 +0100

    pushing the value of input into the myLeads array

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
 let myLeads = [];
+// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 // Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
 inputBtn.addEventListener("click", function () {
-    myLeads.push("www.awesomeleads.com")
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputEl.value)
+    // myLeads.push("www.awesomeleads.com")
     console.log(myLeads)
 })
+
+// Log out the items in the myLeads array using a for loop 
