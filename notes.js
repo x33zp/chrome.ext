b768cd938160a14e2732bd7bad226f1c51e306fb
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-17 17:18:22 +0100

    eventListeners

---
+++ notes.js
@@ -0,0 +1,9 @@
+// 1. Grab the box from the DOM and store it in a variable
+// 2. Add a click event listener to the box 
+// 3. Log out "I want to open the box!" when it's clicked
+
+let boxBtn = document.querySelector("#box")
+
+boxBtn.addEventListener("click", function () {
+    console.log("I want to open the box!")
+})
