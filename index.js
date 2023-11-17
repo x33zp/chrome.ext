92e08ee6cb7f4ec687a03f2fc6af944076696aaf
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-17 17:17:24 +0100

    rendered the leads in the unordered list

--- index.js
+++ index.js
@@ -5,9 +5,7 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
-// 2. Grab the unordered list and store it in a const variable called ulEl
-const ulEL = document.getElementsById("ul-el")
+const ulEl = document.getElementById("ul-el")
 
 // Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
@@ -21,6 +19,7 @@ inputBtn.addEventListener("click", function () {
 })
 
 // Log out the items in the myLeads array using a for loop 
+// Render the leads in the unordered list using ulEl.textContent
 for (i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+    ulEl.textContent += `${myLeads[i]} `
 }
