7bbcb67e21f4036161ba464f6be7a8983b188803
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-17 18:15:07 +0100

    listItems and loop to a renderLeeds function so when button is clicked the list appears beneath

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Create two variables:
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
-// let myLeads = [];
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = [];
+// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -15,15 +15,26 @@ inputBtn.addEventListener("click", function () {
     // Google -> "get value from input field javascript"
     myLeads.push(inputEl.value)
     // myLeads.push("www.awesomeleads.com")
-    console.log(myLeads)
+    // console.log(myLeads)
+    renderLeads()
 })
+
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
 
 // Log out the items in the myLeads array using a for loop 
 // Render the leads in the unordered list using ulEl.textContent
-for (i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
-    /// another method of add lists instead of using .innerHTML
-    const li = document.createElement("li")
-    li.innerText = myLeads[i]
-    ulEl.append(li)
+function renderLeads() {
+    let listItems = ""
+    for (i = 0; i < myLeads.length; i++) {
+        // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
+        /// another method of add lists instead of using .innerHTML
+        // const li = document.createElement("li")
+        // li.innerText = myLeads[i]
+        // ulEl.append(li)
+        // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+        listItems += `<li>${myLeads[i]}</li>`
+    }
+    // 3. Render the listItems inside the unordered list using ulEl.innerHTML
+    ulEl.innerHTML = listItems
 }
