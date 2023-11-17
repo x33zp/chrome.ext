e62c42ca25298049a6792674f6465063744098f3
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-17 16:56:21 +0100

    pushing a value to the myLeades array when button is clicked

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+let myLeads = [];
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
-let inputBtn = document.getElementById("input-btn")
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
 inputBtn.addEventListener("click", function () {
-    console.log("Button clicked!")
+    myLeads.push("www.awesomeleads.com")
+    console.log(myLeads)
 })
