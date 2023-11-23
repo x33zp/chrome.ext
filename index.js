41285b4b6d80a79f3f797e039560f12653bc19c1
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-23 13:07:24 +0100

    function paraments and arguments

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
 // Create two variables:
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
-let myLeads = `[]`;
-
-// 1. Turn the myLeads string into an array
-myLeads = JSON.parse(myLeads)
-// 2. Push a new value to the array
-myLeads.push("new")
-// 3. Turn the array into a string again
-myLeads = JSON.stringify(myLeads)
-// 4. Console.log the string using typeof to verify that it's a string
-console.log(typeof myLeads)
+let myLeads = [];
 
 // let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+// 1. Store the delete button in a deleteBtn variable
+const deleteBtn = document.getElementById("delete-btn")
 
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// console.log(leadsFromLocalStorage)
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-/// 1. Save a key-value pair in localStorage
-// localStorage.setItem("myLeads", "www.exampleleads.com")
-/// 2. Refresh the page. Get the value and log it to the console
-console.log(localStorage.getItem("myLeads"))
-// 3. Clear localStorage
-localStorage.clear("myLeads")
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
 
-// HINTS:
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
-// localStorage.clear()
-// PS: both key and value need to be strings
+// Log out the items in the myLeads array using a for loop 
+// Render the leads in the unordered list using ulEl.textContent
+function render(leads) {
+    let listItems = ""
+    for (i = 0; i < leads.length; i++) {
+        // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
+        /// another method of add lists instead of using .innerHTML
+        // const li = document.createElement("li")
+        // li.innerText = myLeads[i]
+        // ulEl.append(li)
+        // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += `<li><a target="_blank" href="${leads[i]}">${leads[i]}</a></li>`
+        // console.log(listItems)
+    }
+    // 3. Render the listItems inside the unordered list using ulEl.innerHTML
+    ulEl.innerHTML = listItems
+}
 
+// 2. Listen for double clicks on the delete button (google it!)
+// 3. When clicked, clear localStorage, myLeads, and the DOM
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
 
 // Push the value "www.awesomelead.com" to myArray when the input button is clicked
@@ -40,34 +61,18 @@ inputBtn.addEventListener("click", function () {
     // instead of the hard-coded "www.awesomeleads.com" value
     // Google -> "get value from input field javascript"
     myLeads.push(inputEl.value)
-    // myLeads.push("www.awesomeleads.com")
-    // console.log(myLeads)
-    inputEl.value = ''
 
     //clear out the input field
+    inputEl.value = ''
 
-    renderLeads()
+    // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    // console.log(myLeads)
+
+    render(myLeads)
+
+    // To verify that it works:
+    // console.log(localStorage.getItem("myLeads"))
 })
-
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-// Assign it to an empty string to begin with
-
-// Log out the items in the myLeads array using a for loop 
-// Render the leads in the unordered list using ulEl.textContent
-function renderLeads() {
-    let listItems = ""
-    for (i = 0; i < myLeads.length; i++) {
-        // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
-        /// another method of add lists instead of using .innerHTML
-        // const li = document.createElement("li")
-        // li.innerText = myLeads[i]
-        // ulEl.append(li)
-        // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-        // Wrap the lead in an anchor tag (<a>) inside the <li>
-        // Can you make the link open in a new tab?
-        listItems += `<li><a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a></li>`
-        // console.log(listItems)
-    }
-    // 3. Render the listItems inside the unordered list using ulEl.innerHTML
-    ulEl.innerHTML = listItems
-}
--- notes.js
+++ notes.js
@@ -22,6 +22,20 @@ function buy() {
 }
 
 /// local storage
+/// 1. Save a key-value pair in localStorage
+// localStorage.setItem("myLeads", "www.exampleleads.com")
+/// 2. Refresh the page. Get the value and log it to the console
+console.log(localStorage.getItem("myLeads"))
+// 3. Clear localStorage
+localStorage.clear("myLeads")
+
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+//
 let myLeads = `[]`;
 
 // 1. Turn the myLeads string into an array
@@ -32,3 +46,33 @@ myLeads.push("new")
 myLeads = JSON.stringify(myLeads)
 // 4. Console.log the string using typeof to verify that it's a string
 console.log(typeof myLeads)
+
+///function parameters
+const welcomeEl = document.getElementById("welcome-el")
+let greetingTxt = "hello"
+let nameTxt = "zubbby peculiar 👋"
+
+function greetUser(greeting, name) {
+    welcomeEl.textContent = `${greeting}, ${name}`
+}
+
+greetUser(greetingTxt, nameTxt)
+
+//numbers as function parameters
+
+// Create a function, add(), that adds two numbers together and returns the sum
+function add(num1, num2) {
+    sum = num1 + num2
+    return sum
+}
+
+console.log(add(3, 7)) // should log 7
+console.log(add(9, 102)) // should log 111
+
+// Create a function, getFirst(arr), that returns the first item in the array
+function getFirst(arr) {
+    return arr[0]
+}
+// Call it with an array as an argument to verify that it work
+
+console.log(getFirst(["first", "second", "third"]))
